2099738d5fe9b5dead4177118c9fae5dae78fd31
Adam Caldwell <adamcaldwell15@gmail.com>
2024-12-04 10:17:50 +0000

    hopefully successful deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
+import {ReactNode} from 'react';
 
-const RootLayout = ({children}) => {
+const RootLayout = ({children}: {children: ReactNode}) => {
   return (
     <html lang="en">
       <body className="p-0 m-0 font-sans text-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import {retrieve} from "../../retrieve.js";
+
+//type characterType = {
+  //CHARACTER_NAME: string
+  //CHARACTER_ALIGNMENT: string
+  //CHARACTER_IDENTITY: string
+  //CHARACTER_DESCRIPTION: string
+  //CHARACTER_ABILITIES: string[]
+//}
+
 export default async function Home() {
   const response = await retrieve();
   const characters = response.Items;
@@ -8,15 +17,15 @@ export default async function Home() {
   );
 }
 
-function Character(props) {
+function Character({character} : {character: Record<string, string | string[]>}) {
   return (
     <div className=" bg-slate-500 flex flex-row justify-evenly items-center m-5 p-2.5 border-4 border-black">
-      <p className="w-1/5">{props.character.CHARACTER_NAME}</p>
-      <p className="w-1/5">{props.character.CHARACTER_ALIGNMENT}</p>
-      <p className="w-1/5">{props.character.CHARACTER_IDENTITY}</p>
-      <p className="w-1/5">{props.character.CHARACTER_DESCRIPTION}</p>
+      <p className="w-1/5">{character.CHARACTER_NAME}</p>
+      <p className="w-1/5">{character.CHARACTER_ALIGNMENT}</p>
+      <p className="w-1/5">{character.CHARACTER_IDENTITY}</p>
+      <p className="w-1/5">{character.CHARACTER_DESCRIPTION}</p>
       <div className="flex flex-col justify-center items-center w-1/5">
-      {props.character.CHARACTER_ABILITIES.map((ability) => <p key={ability}>{ability}</p>)}
+      {(character.CHARACTER_ABILITIES as string[]).map((ability) => <p key={ability}>{ability}</p>)}
       </div>
     </div>
   )
